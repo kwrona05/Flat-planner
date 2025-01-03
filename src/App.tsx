2fc2f6e9c981326b3d914a5a39a2b41256ff70d4
kwrona05 <kwrona05@gmail.com>
2025-01-03 11:34:46 +0100

    Create main stage without styles

---
+++ src/App.tsx
@@ -0,0 +1,6 @@
+import Main from "./components/Main";
+
+const App: React.FC = () => {
+  return <Main />;
+};
+export default App;
--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -1,20 +1,100 @@
-const Main = () => {
-  const handleButtonClick = () => {
-    <form>
-      <h2>Start</h2>
-      <p>If you want to add a room (shape) click button '+'.</p>
-      <h2>Drag</h2>
-      <p>You can drag shape by handle mouse on it</p>
-      <h2>Remove</h2>
-      <p>You</p>
-    </form>;
+import React, { useState } from "react";
+import { Stage, Layer, Rect, Text } from "react-konva";
+import "../App.css";
+
+type Shape = {
+  id: string;
+  x: number;
+  y: number;
+  width: number;
+  height: number;
+  color: string;
+  type: "square";
+};
+
+const Main: React.FC = () => {
+  const [isOpen, setIsOpen] = useState<boolean>(false);
+  const [isopenLegend, setIsOpenLegend] = useState<boolean>(false);
+  const [shapes, setShapes] = useState<Shape[]>([]);
+
+  const handleDragEnd = (e: any, id: string) => {
+    const updateShapes = shapes.map((shape) =>
+      shape.id === id ? { ...shape, x: e.target.x(), y: e.target.y() } : shape
+    );
+    setShapes(updateShapes);
   };
+
+  const addRect = (type: "square") => {
+    const newShape: Shape = {
+      id: `shape-${shapes.length + 1}`,
+      width: 100,
+      height: 100,
+      x: 50,
+      y: 50,
+      color: "gray",
+      type: "square",
+    };
+    setShapes([...shapes, newShape]);
+  };
+
+  const openForm = (): void => {
+    setIsOpen((prevState) => !prevState);
+  };
+
+  const openLegend = (): void => {
+    setIsOpenLegend((prevState) => !prevState);
+  };
+
   return (
     <div className="container">
       <div className="header">
-        <button>Instruction</button>
+        <button onClick={openForm}>Instruction</button>
+        {isOpen && (
+          <form>
+            <p>You can add new room by clicking '+' button</p>
+            <p>
+              You can remove room by clicking on it and then clicking remove
+            </p>
+            <p>You can drag room on the canvas by holding the mouse on it</p>
+            <p>You can see the legend by clicking 'Legend' button</p>
+            <p>You can download your flat by clicking 'Download' button</p>
+          </form>
+        )}
+        <button onClick={openLegend}>Legend</button>
+        {isopenLegend && (
+          <form>
+            <p>Corridor - Gray</p>
+            <p>Living room - Red</p>
+            <p>Kitchen - Orange</p>
+            <p>Bathroom - Blue</p>
+            <p>Office - Green</p>
+            <p>Bedroom - Yellow</p>
+          </form>
+        )}
+        {/*remove button*/}
+        {/*download button*/}
+        {/*add button*/}
       </div>
-      <div className="body"></div>
+      <div className="stage-container">
+        <button onClick={() => addRect("square")}>+</button>
+        <Stage width={600} height={400}>
+          <Layer>
+            {shapes.map((shape) => (
+              <Rect
+                key={shape.id}
+                x={shape.x}
+                y={shape.y}
+                width={shape.width}
+                height={shape.height}
+                fill={shape.color}
+                draggable
+                onDragEnd={(e) => handleDragEnd(e, shape.id)}
+              />
+            ))}
+          </Layer>
+        </Stage>
+      </div>
     </div>
   );
 };
+export default Main;
