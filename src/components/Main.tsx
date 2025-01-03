f5172a371a4fe40e79b43bb63daf823c19c0d253
kwrona05 <kwrona05@gmail.com>
2025-01-03 09:16:37 +0100

    start creating main component

---
+++ src/components/Main.tsx
@@ -0,0 +1,20 @@
+const Main = () => {
+  const handleButtonClick = () => {
+    <form>
+      <h2>Start</h2>
+      <p>If you want to add a room (shape) click button '+'.</p>
+      <h2>Drag</h2>
+      <p>You can drag shape by handle mouse on it</p>
+      <h2>Remove</h2>
+      <p>You</p>
+    </form>;
+  };
+  return (
+    <div className="container">
+      <div className="header">
+        <button>Instruction</button>
+      </div>
+      <div className="body"></div>
+    </div>
+  );
+};
