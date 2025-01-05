2fec79d4b972d6021fd7b225c9e50550a1641642
kwrona05 <kwrona05@gmail.com>
2025-01-05 12:21:42 +0100

    Change logic in Main.tsx
    Now Main.tsx give Props to Header.tsx so buttons in header work

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -17,7 +17,6 @@ type Shape = {
 const Main: React.FC = () => {
   const [shapes, setShapes] = useState<Shape[]>([]);
   const [selectedId, setSelectedId] = useState<string | null>(null);
-  const [isMenuOpen, setIsMenuOpen] = useState<boolean>(false);
   const stageRef = useRef<any>(null);
   const transformerRef = useRef<any>(null);
 
@@ -33,6 +32,46 @@ const Main: React.FC = () => {
       transformerRef.current.getLayer().batchDraw();
     }
   }, [selectedId]);
+
+  const addRect = (type: "square", name: string, color: string) => {
+    const newShape: Shape = {
+      id: `shape-${shapes.length + 1}`,
+      width: 100,
+      height: 100,
+      x: 50,
+      y: 50,
+      name,
+      color,
+      type,
+    };
+    setShapes([...shapes, newShape]);
+  };
+
+  const removeSelectedShape = () => {
+    if (selectedId) {
+      const updatedShapes = shapes.filter((shape) => shape.id !== selectedId);
+      setShapes(updatedShapes);
+      setSelectedId(null);
+    }
+  };
+
+  const downloadButton = (uri: string, name: string) => {
+    const link = document.createElement("a");
+    link.download = name;
+    link.href = uri;
+    document.body.appendChild(link);
+    link.click();
+    document.body.removeChild(link);
+  };
+
+  const handleExport = () => {
+    if (stageRef.current) {
+      const uri = stageRef.current.toDataURL();
+      downloadButton(uri, "stage-export.png");
+    } else {
+      console.error("stageRef.current is null");
+    }
+  };
 
   const handleDragEnd = (e: any, id: string) => {
     const updatedShapes = shapes.map((shape) =>
@@ -59,89 +98,21 @@ const Main: React.FC = () => {
     node.scaleY(1);
   };
 
-  const openMenuButton = (): void => {
-    setIsMenuOpen((prevState) => !prevState);
-  };
-
-  const addRect = (type: "square", name: string, color: string) => {
-    const newShape: Shape = {
-      id: `shape-${shapes.length + 1}`,
-      width: 100,
-      height: 100,
-      x: 50,
-      y: 50,
-      name: "corridor",
-      color: `${color}`,
-      type: "square",
-    };
-    setShapes([...shapes, newShape]);
-  };
-
-  const removeSelectedShape = () => {
-    if (selectedId) {
-      const updatedShapes = shapes.filter((shape) => shape.id !== selectedId);
-      setShapes(updatedShapes);
-      setSelectedId(null);
-    }
-  };
-
   const handleSelect = (id: string) => {
     setSelectedId(id);
-  };
-
-  const downloadButton = (uri: string, name: string) => {
-    const link = document.createElement("a");
-    link.download = name;
-    link.href = uri;
-    document.body.appendChild(link);
-    link.click();
-    document.body.removeChild(link);
-  };
-
-  const handleExport = () => {
-    if (stageRef.current) {
-      const uri = stageRef.current.toDataURL();
-      downloadButton(uri, "stage-export.png");
-    } else {
-      console.error("stageRef.current is null");
-    }
   };
 
   return (
     <div className="container">
       <div className="header">
-        <Header />
+        <Header
+          addRect={addRect}
+          removeSelectedShape={removeSelectedShape}
+          handleExport={handleExport}
+          selectedId={selectedId}
+        />
       </div>
       <div className="stage-container">
-        <div>
-          <button onClick={openMenuButton}>+</button>
-          {isMenuOpen && (
-            <menu>
-              <button onClick={() => addRect("square", "LivingRoom", "red")}>
-                Living Room
-              </button>
-              <button onClick={() => addRect("square", "Corridor", "gray")}>
-                Corridor
-              </button>
-              <button onClick={() => addRect("square", "Bedroom", "yellow")}>
-                Bedroom
-              </button>
-              <button onClick={() => addRect("square", "Kitchen", "orange")}>
-                Kitchen
-              </button>
-              <button onClick={() => addRect("square", "Bathroom", "blue")}>
-                Bathroom
-              </button>
-              <button onClick={() => addRect("square", "Office", "green")}>
-                Office
-              </button>
-            </menu>
-          )}
-          <button onClick={removeSelectedShape} disabled={!selectedId}>
-            Remove
-          </button>
-          <button onClick={handleExport}>Export</button>
-        </div>
         <Stage width={1000} height={600} ref={stageRef}>
           <Layer>
             {shapes.map((shape) => (
